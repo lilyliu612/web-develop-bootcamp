ea2017d19748da91cb7add4eedcc2bea70b4741f
qzk11 <616341668@qq.com>
2020-02-25 23:08:43 -0500

    1

---
+++ section11/practice.js
@@ -0,0 +1,24 @@
+
+// //if and while
+// var target = 10;
+// var guess = "0";
+// while(Number(guess) !== target) {
+//     guess = prompt("guess");
+//     var tmp = Number(guess);
+//     if(tmp > target){
+//         alert("too high");
+//     }
+//     else if(tmp < target){
+//         alert("too low");
+//     }
+// }
+// alert("yes");
+// var count = 5;
+
+// while(count <= 50) {
+//     if(count % 3 ===0 && count % 5 === 0){
+//         console.log(count);
+//     }
+//     count++;
+// }
+
