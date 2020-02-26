e0b085bda94c894a93b12d03498a2b7bc122f761
qzk11 <616341668@qq.com>
2020-02-25 20:52:35 -0500

    navbar

---
+++ section10/assignment.js
@@ -0,0 +1,5 @@
+var firstname = prompt("input your fn: ");
+var lastname = prompt("input your ln: ");
+var age = prompt("input your age: ");
+console.log("your full name is: " + firstname +" " + lastname);
+console.log("you are " + age + " old");
